e5d34796484f40eb229a88042e97fc0307153bcc
Harinder kaur <snazyahuja11@gmail.com>
2018-11-04 17:48:13 -0500

    Add files and project setup

---
+++ src/main/java/spm/erp/hr/HumanResourceApplication.java
@@ -0,0 +1,12 @@
+package spm.erp.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourceApplication.class, args);
+	}
+}
